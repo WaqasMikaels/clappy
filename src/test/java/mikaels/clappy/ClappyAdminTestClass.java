28c05a666438e1273c4dae95037c3054c3a0e152
WaqasMikaels <waqas.ahmed@mikaels.com>
2018-09-24 19:00:14 +0500

    First push

---
+++ src/test/java/mikaels/clappy/ClappyAdminTestClass.java
@@ -0,0 +1,159 @@
+package mikaels.clappy;
+
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import mikaels.clappy.baseclass.BaseUi;
+import mikaels.clappy.resources.DSLLibDesktop;
+
+public class ClappyAdminTestClass {
+	mikaels.clappy.resources.DSLLibDesktop dsl;
+	  //public String xPath = "D:/Excels/MyExcel.xls";
+	  //public String xPath = "D:/Excels/MyFirstExcel.xlsx";
+	  //public String xlPath_Res = "D:/Excels/userStatuses.xlsx";
+	  //public static String XData[][];
+	  //public String xlData[][];
+	  
+
+	  @BeforeClass
+	  public void beforeClass() throws Exception {
+		  	dsl = new DSLLibDesktop();
+			//dsl.launchCubixCo();
+		  	//dsl.launchGoogle();
+		  	//dsl.xlRead(xPath);
+		  	//dsl.launchFrank();
+		  	dsl.launchClappy();
+	  }
+	  
+	 /* @Test
+	  public void Test01_Verify_Frank_Title() {
+		  dsl.franktestclass.CheckFrankTitle();
+	  }*/
+	  
+	  @Test
+	  public void Test01_Fill_Login_Credentials() {
+		  dsl.clappytestclass.EnterEmailAddress();
+		  dsl.clappytestclass.EnterPassword();
+	  }
+	  
+	  @Test
+	  public void Test02_Click_Login_Button() {
+		  dsl.clappytestclass.ClickLogin();
+	  }
+	  
+	  @Test
+	  public void Test03_Verify_Login() {
+		  dsl.clappytestclass.VerifyLogin();		  
+	  }
+	  
+	  @Test
+	  public void Test04_Redirect_To_Admin_Management() {
+		  dsl.clappytestclass.ClickOnAdminManagement();
+	  }
+	  
+	  @Test
+	  public void Test05_Verify_Redirection_To_Admin_Management() {
+		  dsl.clappytestclass.VerifyRedirectionToAdminManagement();		  
+	  }
+	  
+	  @Test
+	  public void Test06_Verify_8_Admin_Management_Elements() {
+		  dsl.clappytestclass.VerifyAdminManagementElements();
+	  }
+	  
+	  @Test
+	  public void Test07_Create_New_Admin() {
+		  dsl.clappytestclass.ClickNewAdminButton();
+	  }
+	  
+	  @Test
+	  public void Test08_Verify_Redirection_To_Create_Admin_Form() {
+		  dsl.clappytestclass.VerifyRedirectionToCreateAdminForm();		  
+	  }
+	  
+	  @Test
+	  public void Test09_Fill_Create_Admin_Form() {
+		  dsl.clappytestclass.FillCreateAdminForm();		  
+	  }
+	  
+	  @Test
+	  public void Test10_Click_Create_Admin() {
+		  dsl.clappytestclass.ClickCreateAdminButton();
+	  }
+	  
+	  @Test
+	  public void Test11_Verify_Create_Admin_Success_Message() {
+		  dsl.clappytestclass.VerifyCreateAdminSuccessMessage();
+	  }
+	  
+	  @Test
+	  public void Test12_Redirect_To_Client_Management() {
+		  dsl.clappytestclass.ClickOnClientManagement();
+	  }
+	  
+	  @Test
+	  public void Test13_Verify_Redirection_To_Client_Management() {
+		  dsl.clappytestclass.VerifyRedirectionToClientManagement();		  
+	  }
+	  
+	  @Test
+	  public void Test14_Verify_10_Client_Management_Elements() {
+		  dsl.clappytestclass.VerifyClientManagementElements();
+	  }
+	  
+	  @Test
+	  public void Test15_Create_New_Client() {
+		  dsl.clappytestclass.ClickNewClientButton();
+	  }
+	  
+	  @Test
+	  public void Test16_Verify_Redirection_To_Create_Client_Form() {
+		  dsl.clappytestclass.VerifyRedirectionToCreateClientForm();		  
+	  }
+	  
+	  @Test
+	  public void Test17_Fill_Create_Client_Form() {
+		  dsl.clappytestclass.FillCreateClientForm();		  
+	  }
+	  
+	  @Test
+	  public void Test18_Click_Create_Client() {
+		  dsl.clappytestclass.ClickCreateClientButton();
+	  }
+	  
+	  @Test
+	  public void Test19_Verify_Create_Client_Success_Message() {
+		  dsl.clappytestclass.VerifyCreateClientSuccessMessage();
+	  }
+	  
+	  @Test
+	  public void Test20_Redirect_To_Client_Management() {
+		  dsl.clappytestclass.ClickOnModuleManagement();
+	  }
+	  
+	  @Test
+	  public void Test21_Verify_Redirection_To_Modules_Management() {
+		  dsl.clappytestclass.VerifyRedirectionToModulesManagement();		  
+	  }
+	  
+	  @Test
+	  public void Test22_Verify_3_Modules_Management_Elements() {
+		  dsl.clappytestclass.VerifyModuleManagementElements();
+	  }
+	  
+	  @Test
+	  public void Test23_Verify_6_Modules_Management_Modules() {
+		  dsl.clappytestclass.VerifyModuleManagementModules();
+	  }
+	  
+	  @AfterClass
+	  public void afterClass() throws Exception {
+		  dsl.closeBrowser();
+		  //xlwrite(xlPath_Res, XData);//Writes the results
+		  //BaseUi.xlwrite(xlPath_Res, XData);
+		  
+	  }
+}
---
+++ src/test/java/mikaels/clappy/ui/ClappyTestClass_UI.java
@@ -0,0 +1,335 @@
+package mikaels.clappy.ui;
+
+import junit.framework.Assert;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+import org.testng.internal.Yaml;
+
+import mikaels.clappy.baseclass.BaseUi;
+
+public class ClappyTestClass_UI extends BaseUi {
+	WebDriver driver;
+	
+
+	public ClappyTestClass_UI(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+	}
+	
+	@FindBy(css = "[id = 'email']")
+	private WebElement txtEmail;
+	
+	@FindBy(css = "[id = 'password']")
+	private WebElement txtPassword;
+	
+	@FindBy(xpath = "//html/body/div/div/div/div/form/div[4]/button")
+	private WebElement btnLogin;
+	
+	@FindBy(xpath = "//*[@id=\"left-col\"]/ul/a[2]/li")
+	private WebElement menuAdminManagement;
+	
+	@FindBy(xpath = "//*[@id=\"left-col\"]/ul/a[3]/li")
+	private WebElement menuClientManagement;
+	
+	@FindBy(xpath = "//*[@id=\"left-col\"]/ul/a[4]/li")
+	private WebElement menuModulesManagement;
+	
+	@FindBy(xpath = "//*[@id=\"left-col\"]/ul/a[5]/li")
+	private WebElement menuSystemSettings;
+	
+	@FindBy(css = "[id = 'newAdminBtn']")
+	private WebElement btnNewAdmin;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[1]")
+	private WebElement colIdAM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[2]")
+	private WebElement colFullNameAM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[3]")
+	private WebElement colEmailAddressAM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[4]")
+	private WebElement colStateAM;
+	
+	@FindBy(css = "[class = 'admin_checkbox']")
+	private WebElement chkbxStateAM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[5]/a/button")
+	private WebElement btnEditAdminAM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[5]/button")
+	private WebElement btnDeleteAdminAM;
+	
+	@FindBy(css = "[id = 'fullName']")
+	private WebElement txtAdminFullNameAM;
+	
+	@FindBy(css = "[id = 'email']")
+	private WebElement txtAdminEmailAM;
+	
+	@FindBy(css = "[id = 'pwd']")
+	private WebElement txtAdminPwdAM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/div/div/form/button")
+	private WebElement btnCreateAdmin;
+	
+	@FindBy(css = "[class = 'alert alert-success']")
+	private WebElement msgCreateAdminSuccess;
+	
+	@FindBy(css = "[id = 'newAdminBtn']")
+	private WebElement btnNewClient;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[1]")
+	private WebElement colIdCM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[2]")
+	private WebElement colFullNameCM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[3]")
+	private WebElement colEmailAddressCM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[4]")
+	private WebElement colSubDomainCM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[5]")
+	private WebElement colStateCM;
+	
+	@FindBy(css = "[class = 'client_checkbox']")
+	private WebElement chkbxStateCM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[6]/a[1]/button")
+	private WebElement btnLoginCM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[6]/a[2]/button")
+	private WebElement btnEditClientCM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[6]/button")
+	private WebElement btnDeleteClientCM;
+	
+	@FindBy(css = "[id = 'fullName']")
+	private WebElement txtAdminFullNameCM;
+	
+	@FindBy(css = "[id = 'email']")
+	private WebElement txtAdminEmailCM;
+	
+	@FindBy(css = "[id = 'pwd']")
+	private WebElement txtAdminPwdCM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/div/div/form/button")
+	private WebElement btnCreateClient;
+	
+	@FindBy(css = "[class = 'alert alert-success']")
+	private WebElement msgCreateClientSuccess;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[1]")
+	private WebElement colIdMM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[2]")
+	private WebElement colModuleNameMM;
+	
+	@FindBy(xpath = "//*[@id=\"right-col\"]/table/thead/tr/th[3]")
+	private WebElement colStateMM;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[1]/td[2]")
+	private WebElement modulePoll;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[2]/td[2]")
+	private WebElement moduleAnimateReactions;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[3]/td[2]")
+	private WebElement moduleCommenting;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[4]/td[2]")
+	private WebElement modulePaidVoting;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[5]/td[2]")
+	private WebElement moduleBannerAds;
+	
+	@FindBy(xpath = "//*[@id=\"append-users-here\"]/tr[6]/td[2]")
+	private WebElement moduleConvertBrandedInteration;
+	
+	
+	
+	public void CheckClappyTitle() {
+		Assert.assertEquals("Frank.ai", driver.getTitle()); 
+		// OR
+		// isElementDisplayed(googleTitle);
+	}
+	
+	public void EnterEmailAddress() {
+		hardwait(5);
+		txtEmail.sendKeys("admin1@clappy.com");
+		hardwait(1);
+	}
+	
+	public void EnterPassword() {
+		txtPassword.sendKeys("12345678");
+		hardwait(1);
+	}
+	
+	public void ClickLogin() {
+		btnLogin.click();
+		hardwait(5);
+	}
+	
+	public void VerifyLogin() {
+		String lnkDashboard = driver.getCurrentUrl();
+		assertEquals(lnkDashboard, "http://stage.clappy.live/admin/dashboard");
+		hardwait(1);
+	}
+	
+	public void ClickOnAdminManagement() {
+		menuAdminManagement.click();
+		hardwait(2);
+	}
+	
+	public void VerifyRedirectionToAdminManagement() {
+		String lnkAdminManagement = driver.getCurrentUrl();
+		assertEquals(lnkAdminManagement, "http://stage.clappy.live/admin/all");
+		hardwait(1);
+	}
+	
+	public void VerifyAdminManagementElements() {
+		isElementDisplayed(btnNewAdmin);
+		isElementDisplayed(colIdAM);
+		isElementDisplayed(colFullNameAM);
+		isElementDisplayed(colEmailAddressAM);
+		isElementDisplayed(colStateAM);
+		isElementDisplayed(chkbxStateAM);
+		isElementDisplayed(btnEditAdminAM);
+		isElementDisplayed(btnDeleteAdminAM);
+	}
+	
+	public void ClickNewAdminButton() {
+		btnNewAdmin.click();
+		hardwait(3);
+	}
+	
+	public void VerifyRedirectionToCreateAdminForm() {
+		String lnkCreateAdminForm = driver.getCurrentUrl();
+		assertEquals(lnkCreateAdminForm, "http://stage.clappy.live/admin/user/add");
+		hardwait(1);
+	}
+	
+	public void FillCreateAdminForm() {
+		txtAdminFullNameAM.sendKeys("AutoAdmin");
+		txtAdminEmailAM.sendKeys("autoadmin"+currentDate()+"@clappy.com");
+		hardwait(1);
+		txtAdminPwdAM.sendKeys("12345678");
+	}
+	
+	public void ClickCreateAdminButton() {
+		btnCreateAdmin.click();
+		hardwait(5);
+	}
+	
+	public void VerifyCreateAdminSuccessMessage() {
+		isElementDisplayed(msgCreateAdminSuccess);
+	}
+	
+	public void ClickOnClientManagement() {
+		menuClientManagement.click();
+		hardwait(2);
+	}
+
+	public void VerifyRedirectionToClientManagement() {
+		String lnkClientManagement = driver.getCurrentUrl();
+		assertEquals(lnkClientManagement, "http://stage.clappy.live/admin/clients");
+		hardwait(1);
+	}
+	
+	public void VerifyClientManagementElements() {
+		isElementDisplayed(btnNewClient);
+		isElementDisplayed(colIdCM);
+		isElementDisplayed(colFullNameCM);
+		isElementDisplayed(colEmailAddressCM);
+		isElementDisplayed(colSubDomainCM);
+		isElementDisplayed(colStateCM);
+		isElementDisplayed(chkbxStateCM);
+		isElementDisplayed(btnLoginCM);
+		isElementDisplayed(btnEditClientCM);
+		isElementDisplayed(btnDeleteClientCM);
+	}
+	
+	public void ClickNewClientButton() {
+		btnNewClient.click();
+		hardwait(3);
+	}
+	
+	public void VerifyRedirectionToCreateClientForm() {
+		String lnkCreateClientForm = driver.getCurrentUrl();
+		assertEquals(lnkCreateClientForm, "http://stage.clappy.live/admin/client/add");
+		hardwait(1);
+	}
+	
+	public void FillCreateClientForm() {
+		txtAdminFullNameCM.sendKeys("AutoClient");
+		txtAdminEmailCM.sendKeys("autoclient"+currentDate()+"@clappy.com");
+		hardwait(1);
+		txtAdminPwdCM.sendKeys("12345678");
+	}
+	
+	public void ClickCreateClientButton() {
+		btnCreateClient.click();
+		hardwait(5);
+	}
+	
+	public void VerifyCreateClientSuccessMessage() {
+		isElementDisplayed(msgCreateAdminSuccess);
+	}
+	
+	public void ClickOnModuleManagement() {
+		menuModulesManagement.click();
+		hardwait(2);
+	}
+	
+	public void VerifyRedirectionToModulesManagement() {
+		String lnkModulesManagement = driver.getCurrentUrl();
+		assertEquals(lnkModulesManagement, "http://stage.clappy.live/admin/modules");
+		hardwait(1);
+	}
+	
+	public void VerifyModuleManagementElements() {
+		isElementDisplayed(colIdMM);
+		isElementDisplayed(colModuleNameMM);
+		isElementDisplayed(colStateMM);
+	}
+	
+	public void VerifyModuleManagementModules() {
+		isElementDisplayed(modulePoll);
+		isElementDisplayed(moduleAnimateReactions);
+		isElementDisplayed(moduleCommenting);
+		isElementDisplayed(modulePaidVoting);
+		isElementDisplayed(moduleBannerAds);
+		isElementDisplayed(moduleConvertBrandedInteration);
+	}
+	
+	
+	
+	/*public void CheckFooterCountryName() {
+		isElementDisplayed(countryName);
+		String footerCountryName = countryName.getText();
+		if (footerCountryName.contentEquals("Pakistan")) {
+			System.out.println("Country Name found");
+		 
+		}else {
+			System.out.println("Country Name not found");
+			
+		}
+	}*/
+	
+	
+}
